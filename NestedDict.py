6a4e2576fec16cfd297a3e7892b4f290acb3fb65
Yousef Azizi <yousef.azizi1982@gmail.com>
2022-04-21 02:55:09 +0430

    convert main method approach to a pattern design form using type of input

--- NestedDict.py
+++ NestedDict.py
@@ -1,17 +1,22 @@
 class nested_keys:
     @staticmethod
     def main(item, all_key, all_values, root):
-        if isinstance(item, dict):
-            nested_keys._dic_itration(item, all_key, all_values, root)
-        elif isinstance(item, list):
-            nested_keys._list_iteration(item, all_key, all_values, root)
-
-        else:
-            all_key.append(root)
-            all_values.append(item)
+        method_name = '_' + type(item).__name__ + '_iteration' if type(item).__name__ in ['list',
+                                                                                          'dict'] else '_' + 'other' + '_iteration'
+        getattr(nested_keys, method_name)(item, all_key, all_values, root)
+        ## without method_name version###
+        # if isinstance(item, dict):
+        #     nested_keys._dict_iteration(item, all_key, all_values, root)
+        # elif isinstance(item, list):
+        #     nested_keys._list_iteration(item, all_key, all_values, root)
+        #
+        # else:
+        #     nested_keys._other_iteration(item, all_values, all_key, root)
+        # all_key.append(root)
+        # all_values.append(item)
 
     @staticmethod
-    def _dic_itration(item, all_key, all_values, root):
+    def _dict_iteration(item, all_key, all_values, root):
         for items in item.keys():
             root_item = root.copy()
             root_item.append(items)
@@ -26,7 +31,14 @@ class nested_keys:
             elif isinstance(items, list):
                 nested_keys.main(items, all_key, all_values, root)
             else:
-                all_key.append(root)
-                all_values.append(items)
+                nested_keys._other_iteration(items, all_values, all_key, root)
+                # all_key.append(root)
+                # all_values.append(items)
             # root = []
+        return all_values, all_key, root
+
+    @staticmethod
+    def _other_iteration(item, all_key, all_values, root):
+        all_key.append(root)
+        all_values.append(item)
         return all_values, all_key, root
